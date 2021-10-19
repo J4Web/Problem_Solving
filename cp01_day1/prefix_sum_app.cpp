d2261e59475bd2f4f5c3917bf4537e95fe285fd9
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2021-10-19 10:19:27 +0530

    Time-complex written

--- cp01_day1/prefix_sum_app.cpp
+++ cp01_day1/prefix_sum_app.cpp
@@ -5,6 +5,9 @@
 #define ll long long
 #define speed ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 using namespace std;
+// 0(n)+0(n)+0(Q)
+// 2 * 0(n)+0(Q)
+// 2 * 10^5+10^4
 int main()
 {
     speed
@@ -13,19 +16,19 @@ int main()
     cin>>n;
     int a[n];
     for(auto &i:a)
-        cin>>i;
+        cin>>i; 0(n)
     vector<int> preSum;
     int sum=0;
     for(auto i:a)
     { 
-        sum+=i;
+        sum+=i; 0(n)
         preSum.pb(sum);
     }
     int q;
     cin>>q;
     while(q--)
     {
-        int l,r;
+        int l,r; 0(q);
         cin>>l>>r;
         if(l-1<0)
             cout<<preSum[r]<<e;
